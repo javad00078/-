2127ca5e5c01935696d42355138d7bd63c1bddea
javad00078 <z.0123546789a.z@gmail.com>
2026-09-16 09:11:30 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,23 @@
-import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({plugins:[react()]});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: [
+      '1-7drt.onrender.com'
+    ]
+  },
+
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      '1-7drt.onrender.com'
+    ]
+  }
+})
